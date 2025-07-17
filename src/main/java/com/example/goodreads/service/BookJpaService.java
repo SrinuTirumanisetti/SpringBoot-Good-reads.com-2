ec3d3944bf4b7399f18a569d945fefc65a296459
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-07-17 00:25:52 +0000

    Integrated addBook() API after Author association

--- src/main/java/com/example/goodreads/service/BookJpaService.java
+++ src/main/java/com/example/goodreads/service/BookJpaService.java
@@ -4,8 +4,10 @@ import com.example.goodreads.model.Book;
 import com.example.goodreads.model.Publisher;
 import com.example.goodreads.model.Author;
 import com.example.goodreads.repository.BookJpaRepository;
+import com.example.goodreads.repository.AuthorJpaRepository;
 import com.example.goodreads.repository.PublisherJpaRepository;
 import com.example.goodreads.repository.BookRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -23,91 +25,83 @@ public class BookJpaService implements BookRepository {
     @Autowired
     private PublisherJpaRepository publisherJpaRepository;
 
+    @Autowired
+    private AuthorJpaRepository authorJpaRepository;
+
     @Override
     public ArrayList<Book> getBooks() {
         List<Book> bookList = bookJpaRepository.findAll();
-        ArrayList<Book> books = new ArrayList<>(bookList);
-        return books;
+        return new ArrayList<>(bookList);
     }
 
     @Override
     public Book getBookById(int bookId) {
-        try {
-            Book book = bookJpaRepository.findById(bookId).get();
-            return book;
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
+        return bookJpaRepository.findById(bookId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @Override
     public Book addBook(Book book) {
-        Publisher publisher = book.getPublisher();
-        int publisherId = publisher.getPublisherId();
-        try{
-
-            publisher = publisherJpaRepository.findById(publisherId).get();
-            book.setPublisher(publisher);
-            bookJpaRepository.save(book);
-            return book;
-        } catch (Exception e) {
-
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Wrong publisherId");
+        List<Integer> authorIds = new ArrayList<>();
+        for (Author author : book.getAuthors()) {
+            authorIds.add(author.getAuthorId());
         }
+
+        int publisherId = book.getPublisher().getPublisherId();
+
+        List<Author> complete_authors = authorJpaRepository.findAllById(authorIds);
+        if (authorIds.size() != complete_authors.size()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "One or more author ids are invalid");
+        }
+
+        Publisher publisher = publisherJpaRepository.findById(publisherId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Wrong publisherId"));
+
+        book.setAuthors(complete_authors);
+        book.setPublisher(publisher);
+        return bookJpaRepository.save(book);
     }
 
     @Override
     public Book updateBook(int bookId, Book book) {
+        Book newBook = bookJpaRepository.findById(bookId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
-        try {
-            Book newBook = bookJpaRepository.findById(bookId).get();
-            if (book.getName() != null) {
-                newBook.setName(book.getName());
-            }
-            if (book.getImageUrl() != null) {
-                newBook.setImageUrl(book.getImageUrl());
-            }
-            if (book.getPublisher() != null) {
-                Publisher publisher = book.getPublisher();
-                int publisherId = publisher.getPublisherId();
-                Publisher newPublisher = publisherJpaRepository.findById(publisherId).get();
-                newBook.setPublisher(newPublisher);
-            }
-            bookJpaRepository.save(newBook);
-            return newBook;
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        if (book.getName() != null) {
+            newBook.setName(book.getName());
         }
+        if (book.getImageUrl() != null) {
+            newBook.setImageUrl(book.getImageUrl());
+        }
+        if (book.getPublisher() != null) {
+            int publisherId = book.getPublisher().getPublisherId();
+            Publisher newPublisher = publisherJpaRepository.findById(publisherId)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Wrong publisherId"));
+            newBook.setPublisher(newPublisher);
+        }
+
+        return bookJpaRepository.save(newBook);
     }
 
     @Override
     public void deleteBook(int bookId) {
-        try {
-            bookJpaRepository.deleteById(bookId);
-        } catch (Exception e) {
+        if (!bookJpaRepository.existsById(bookId)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
-        throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+        bookJpaRepository.deleteById(bookId);
     }
 
     @Override
     public Publisher getBookPublisher(int bookId) {
-        try {
-            Book book = bookJpaRepository.findById(bookId).get();
-            return book.getPublisher();
-        } catch(Exception e){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
+        Book book = bookJpaRepository.findById(bookId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return book.getPublisher();
     }
 
     @Override
-    public List<Author> getBookAuthors(int bookId){
-        try{
-           Book book = bookJpaRepository.findById(bookId).get();
-           return book.getAuthors(); 
-        }
-        catch(Exception e){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
+    public List<Author> getBookAuthors(int bookId) {
+        Book book = bookJpaRepository.findById(bookId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return book.getAuthors();
     }
 }
